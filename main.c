ed050250a46580c2805e36d0b65caceff35d928a
ygang9626-arch <ygang9626@gmail.com>
2026-04-06 01:29:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
   파일이름: 익명 프로필 만들기
   작 성 자: 60251629 강영현
 */
-include<stdio.h>
+#include<stdio.h>
+#include <stdlib.h>
 
 
 int main()
 {
-
+	system("chcp 65001");
 	
 	char initial; // 이니셜 
 	float height; // 키 
